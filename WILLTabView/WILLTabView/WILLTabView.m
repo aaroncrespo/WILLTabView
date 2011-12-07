8ed71b5a8fe4ca9a8ae5a49e5ed96860b9653ce1
Aaron Crespo <aaroncrespo@gmail.com>
2011-12-07 17:07:37 -0500

    It all looks nice, behind the scenes it could be better.

--- WILLTabView/WILLTabView/WILLTabView.m
+++ WILLTabView/WILLTabView/WILLTabView.m
@@ -3,51 +3,51 @@
 #import <Cocoa/Cocoa.h>
 #import <QuartzCore/QuartzCore.h>
 
-#define TAB_WIDTH       20.0f
-#define TAB_HEIGHT      20.0f
-#define BAR_HEIGHT      25.0f
+#define TAB_WIDTH       25.0f
+#define TAB_HEIGHT      24.0f
+#define BAR_HEIGHT      28.0f
 #define LEFT_PADDING    25.0f
 #define BORDER_COLOR				[NSColor colorWithCalibratedWhite:(167/255.0f) alpha:1]
 
 @implementation WILLTabCell
-//- (void) drawInteriorWithFrame:(NSRect)cellFrame inView:(NSView *)controlView {
-//    [super drawInteriorWithFrame:cellFrame inView:controlView];
-//}
-//
-
-//- (void) drawWithFrame:(NSRect)cellFrame inView:(NSView *)controlView {
-//    [super drawWithFrame:cellFrame inView:controlView];
-//}  Because drawWithFrame:inView: invokes drawInteriorWithFrame:inView: after it draws the cell's border, do not invoke drawWithFrame:inView: in your override implementation.
-
 - (void) drawSegment:(NSInteger)segment inFrame:(NSRect)frame withView:(NSView *)controlView
 {
-    [super drawSegment:segment inFrame:frame withView:controlView];
 
-    frame.origin.x = segment * TAB_WIDTH +100;
+    frame.origin.x = segment * TAB_WIDTH;
     frame.origin.y = (BAR_HEIGHT - TAB_HEIGHT) /2 ;
     frame.size.width = TAB_WIDTH;
     frame.size.height = TAB_HEIGHT;
     
+    //there should be a way to remove the old control and transfer the event handling to the new one.
+    [super setWidth:TAB_WIDTH -4 forSegment:segment];
+
     NSImage *buttonImage;
-    if(([self selectedSegment] == segment) | [self isHighlighted]) 
+    if([self isSelectedForSegment:segment]) 
     {
+        buttonImage = [NSImage imageNamed:@"LeftNavButtonPressed"];
+    }
+    else if ([self isHighlighted]) {
+        //may want a diferent image just for highlight
         buttonImage = [NSImage imageNamed:@"LeftNavButtonPressed"];
     }
     else 
     {
         buttonImage = [NSImage imageNamed:@"LeftNavButton"];
     }
+    //Can substitute with NSDrawThreePartImage if needed.
+
 	[buttonImage setFlipped:[[self controlView] isFlipped]];
 
     //need to add padding 
 	[buttonImage drawInRect:frame fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1];
+    
 
-    NSLog(@"%@", [[super imageForSegment:segment]name]);
-    [[self imageForSegment:segment] drawInRect:frame fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1];
-
-    //Can substitute with NSDrawThreePartImage if needed.
+    //Im sure theres a better way of doing this.
+    [[super imageForSegment:segment]setFlipped:YES];
+    [[super imageForSegment:segment] drawInRect:NSMakeRect(frame.origin.x+7, frame.origin.y+4, frame.size.width -14, frame.size.height-8)
+                                       fromRect:NSZeroRect 
+                                      operation:NSCompositeSourceOver fraction:1];
 }
-
 @end
 
 @class WILLTabCell;
@@ -58,12 +58,10 @@
 
 -(void)awakeFromNib {
     [segmentedControl setHidden:YES];
-     
-
     [segmentedControl setSegmentCount:[self tabViewItems].count];
     [segmentedControl setTarget:self];
     [segmentedControl setAction:@selector(ctrlSelected:)];
-
+    
     //sync external control to the internal
     [segmentedControl setSelectedSegment:[self indexOfTabViewItem:[self selectedTabViewItem]]];
 
