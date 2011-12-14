426f0b4e28ee1d2eef5e7da5f8a87232bba1cb8e
Panupan Sriautharawong <panupan@willinteractive.com>
2011-12-14 12:00:17 -0500

    using LEFT_PADDING const

--- WILLTabView/WILLTabView/WILLTabView.m
+++ WILLTabView/WILLTabView/WILLTabView.m
@@ -14,7 +14,7 @@
     [segmentedControl setAction:@selector(ctrlSelected:)];
     //sync external control to the internal
     [segmentedControl setSelectedSegment:[self indexOfTabViewItem:[self selectedTabViewItem]]];
-    [segmentedControl setFrame:NSMakeRect(20, self.bounds.size.height-BAR_HEIGHT, self.bounds.size.width-20, BAR_HEIGHT)];
+    [segmentedControl setFrame:NSMakeRect(LEFT_PADDING, self.bounds.size.height-BAR_HEIGHT, self.bounds.size.width-LEFT_PADDING, BAR_HEIGHT)];
     
     //content clipping.
     [self setFrameSize:NSMakeSize(_bounds.size.width, _bounds.size.height - BAR_HEIGHT)];
@@ -33,7 +33,7 @@
 -(void)drawRect:(NSRect)dirtyRect {
     NSImage *barImage = [NSImage imageNamed:@"WILLTabViewBG"];
     [barImage setFlipped:TRUE];
-    [barImage drawInRect:NSMakeRect(0, self.bounds.size.height-BAR_HEIGHT, self.bounds.size.width-20, BAR_HEIGHT)
+    [barImage drawInRect:NSMakeRect(0, self.bounds.size.height-BAR_HEIGHT, self.bounds.size.width-LEFT_PADDING, BAR_HEIGHT)
                 fromRect:NSZeroRect 
                operation:NSCompositeSourceAtop fraction:1];
  
