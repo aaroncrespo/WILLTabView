19eb7798b97d56d7584df3412eda41ab367e88a8
Aaron Crespo <aaroncrespo@gmail.com>
2011-12-15 15:57:00 -0500

    some reoganization

--- WILLTabView/WILLTabView/WILLTabView.m
+++ WILLTabView/WILLTabView/WILLTabView.m
@@ -17,21 +17,25 @@
 
     // Setup segmented control
     segmentedControl = [[NSSegmentedControl alloc] init];
-    [segmentedControl setCell:[[WILLTabCell alloc] init]];    
+    [segmentedControl setSegmentStyle:NSSegmentStyleTexturedSquare];
+    [segmentedControl setCell:[[WILLTabCell alloc] init]];  
     [segmentedControl setSegmentCount:self.numberOfTabViewItems];   
     [segmentedControl setTarget:self];
     [segmentedControl setAction:@selector(ctrlSelected:)];
-    [segmentedControl setSelectedSegment:[self indexOfTabViewItem:[self selectedTabViewItem]]];
+
     for (int i=0; i < self.numberOfTabViewItems; i++) {
-        //[segmentedControl setLabel:[[self tabViewItemAtIndex:i] label] forSegment:i];
         [segmentedControl setImage:[NSImage imageNamed:[[self tabViewItemAtIndex:i] label]] forSegment:i];
     } 
+
     [segmentedControl setFrame:NSMakeRect(LEFT_PADDING, 0, self.frame.size.width, BAR_HEIGHT)];
     [segmentedControl setAutoresizingMask:NSViewMinYMargin];
+
     [barView addSubview:segmentedControl];
 
     //clipping
     [self setFrameSize:NSMakeSize([self bounds].size.width, [self bounds].size.height - BAR_HEIGHT)];
+    [segmentedControl setSelectedSegment:[self indexOfTabViewItem:[self selectedTabViewItem]]];
+
 }
 
 #pragma mark Callback - link our segementedControl to the tabViewItems
@@ -40,32 +44,32 @@
     [super selectTabViewItemAtIndex:[sender selectedSegment]];
 }
 
-#pragma mark segment control and tabview sync methods
+#pragma mark Keep tabs on what gets selected - so our own segmentedControl stays synced.
 
 -(void)selectTabViewItem:(NSTabViewItem *)tabViewItem {
-   [super selectTabViewItem:tabViewItem];
-   [segmentedControl setSelectedSegment:[self indexOfTabViewItem:[self selectedTabViewItem]]];
+    [super selectTabViewItem:tabViewItem];
+    [segmentedControl setSelectedSegment:[self indexOfTabViewItem:[self selectedTabViewItem]]];
 }
 
 -(void)selectTabViewItemAtIndex:(NSInteger)index {
-   [super selectTabViewItemAtIndex:index];
-   [segmentedControl setSelectedSegment:[self indexOfTabViewItem:[self selectedTabViewItem]]];
+    [super selectTabViewItemAtIndex:index];
+    [segmentedControl setSelectedSegment:[self indexOfTabViewItem:[self selectedTabViewItem]]];
 }
 
 -(void)selectTabViewItemWithIdentifier:(id)identifier {
-   [super selectTabViewItemWithIdentifier:identifier];
-   [segmentedControl setSelectedSegment:[self indexOfTabViewItem:[self selectedTabViewItem]]];    
+    [super selectTabViewItemWithIdentifier:identifier];
+    [segmentedControl setSelectedSegment:[self indexOfTabViewItem:[self selectedTabViewItem]]];    
 }
 // skipping selectNext/PreviousTabViewItem - hoping they use above.
 
 -(void)addTabViewItem:(NSTabViewItem *)anItem {
-   [self addTabViewItem:anItem];
+   [super addTabViewItem:anItem];
    [self awakeFromNib];
    [self setNeedsDisplay:YES];
 }
 
 -(void)removeTabViewItem:(NSTabViewItem *)anItem {
-   [self removeTabViewItem:anItem];
+   [super removeTabViewItem:anItem];
    [self awakeFromNib];
    [self setNeedsDisplay:YES];
 }
