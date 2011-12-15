f9e4cf766d8d207b317dee20fcafb0d3e2c42e8c
Panupan Sriautharawong <panupan@willinteractive.com>
2011-12-15 16:07:32 -0500

    oops

--- WILLTabView/WILLTabView/WILLTabView.m
+++ WILLTabView/WILLTabView/WILLTabView.m
@@ -18,12 +18,12 @@
     for (int i=0; i < self.numberOfTabViewItems; i++) {
         [segmentedControl setImage:[NSImage imageNamed:[[self tabViewItemAtIndex:i] label]] forSegment:i];
     } 
-    [segmentedControl setFrame:NSMakeRect(LEFT_PADDING, 5, self.frame.size.width, BAR_HEIGHT)];
+    [segmentedControl setFrame:NSMakeRect(LEFT_PADDING, 0, self.frame.size.width, BAR_HEIGHT)];
     [segmentedControl setAutoresizingMask:NSViewMinYMargin];
     [self addSubview:segmentedControl];
     
-	[self setTabViewType:NSNoTabsNoBorder];
-	//[self setDrawsBackground:NO];
+	[self setTabViewType:NSNoTabsNoBorder];    
+	[self setDrawsBackground:NO];
 }
 
 - (NSSize)minimumSize {
