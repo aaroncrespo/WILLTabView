3f90aae6e47cc1d7bd3f267623d51b532854e28f
Panupan Sriautharawong <panupan@willinteractive.com>
2011-12-16 18:07:42 -0500

    starting subtab view

---
+++ WILLTabView/WILLTabView/WILLSubTabView.h
@@ -0,0 +1,16 @@
+//
+//  WILLSubTabView.h
+//  WILLTabView
+//
+//  Created by Panupan Sriautharawong on 12/16/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface WILLSubTabView : NSTabView {
+    NSSegmentedControl *segmentedControl;    
+    NSImage *barImage;
+}
+
+@end
---
+++ WILLTabView/WILLTabView/WILLSubTabView.m
@@ -0,0 +1,81 @@
+#import "WILLSubTabView.h"
+
+@implementation WILLSubTabView
+
+-(id)initWithCoder:(NSCoder *)aDecoder {
+    if (self = [super initWithCoder:aDecoder]) {
+        barImage = [NSImage imageNamed:@"WILLTabViewBG"];
+        // Setup segmented control
+        segmentedControl = [[NSSegmentedControl alloc] init];
+        //[segmentedControl setCell:[[WILLTabCell alloc] init]];    
+        [segmentedControl setSegmentCount:self.numberOfTabViewItems];   
+        [segmentedControl setTarget:self];
+        [segmentedControl setAction:@selector(ctrlSelected:)];
+        [segmentedControl setSelectedSegment:[self indexOfTabViewItem:[self selectedTabViewItem]]];
+        for (int i=0; i < self.numberOfTabViewItems; i++) {
+            [segmentedControl setLabel:[[self tabViewItemAtIndex:i] label] forSegment:i];
+        } 
+        [segmentedControl setSegmentStyle:NSSegmentStyleRoundRect];
+        [segmentedControl setAutoresizingMask:NSViewMinYMargin];
+        [self addSubview:segmentedControl];
+        
+        [self setTabViewType:NSNoTabsNoBorder];    
+        [self setDrawsBackground:NO];
+        
+        [segmentedControl setFrame:NSMakeRect(20, 0, self.frame.size.width, barImage.size.height)];
+    }
+    
+    return self;
+}
+
+- (NSSize)minimumSize {
+    return NSMakeSize(0, barImage.size.height);
+}
+
+- (NSRect)contentRect {
+    return NSMakeRect(0, barImage.size.height, self.frame.size.width, self.frame.size.height-barImage.size.height);
+}
+
+#pragma mark Callback - link our segementedControl to the tabViewItems
+
+-(void)ctrlSelected:(NSSegmentedControl *)sender {
+    [super selectTabViewItemAtIndex:[sender selectedSegment]];
+}
+
+#pragma mark segment control and tabview sync methods
+
+-(void)selectTabViewItem:(NSTabViewItem *)tabViewItem {
+    [super selectTabViewItem:tabViewItem];
+    [segmentedControl setSelectedSegment:[self indexOfTabViewItem:[self selectedTabViewItem]]];
+}
+
+-(void)selectTabViewItemAtIndex:(NSInteger)index {
+    [super selectTabViewItemAtIndex:index];
+    [segmentedControl setSelectedSegment:[self indexOfTabViewItem:[self selectedTabViewItem]]];
+}
+
+-(void)selectTabViewItemWithIdentifier:(id)identifier {
+    [super selectTabViewItemWithIdentifier:identifier];
+    [segmentedControl setSelectedSegment:[self indexOfTabViewItem:[self selectedTabViewItem]]];    
+}
+// skipping selectNext/PreviousTabViewItem - hoping they use above.
+
+-(void)addTabViewItem:(NSTabViewItem *)anItem {
+    [super addTabViewItem:anItem];
+    [self awakeFromNib];
+    [self setNeedsDisplay:YES];
+}
+
+-(void)removeTabViewItem:(NSTabViewItem *)anItem {
+    [super removeTabViewItem:anItem];
+    [self awakeFromNib];
+    [self setNeedsDisplay:YES];
+}
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [barImage setFlipped:YES];
+    [barImage drawInRect:NSMakeRect(0, 0, self.frame.size.width, barImage.size.height)
+                fromRect:NSZeroRect 
+               operation:NSCompositeSourceOver fraction:1];
+}
+@end
